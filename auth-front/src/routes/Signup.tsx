6758d481b6986ecf7a3c5886a551b4134e76c994
Byron2016 <bgva2005@yahoo.com>
2024-01-30 18:31:01 -0500

    add a DefaultLayout and use it with Login and Signup

--- auth-front/src/routes/Signup.tsx
+++ auth-front/src/routes/Signup.tsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import DefaultLayout from "../layout/DefaultLayout"
+
 export default function Signup(){
-  return <h1>Signup</h1>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  return (
+    <DefaultLayout>
+      <form className="form">
+          <h1>Signup</h1>
+          <label >Name</label>
+          <input type="text" value={name} 
+          onChange={(e) => setName(e.target.value)}/>
+
+          <label >UserName</label>
+          <input type="text"  value={username} 
+          onChange={(e) => setUsername(e.target.value)}/>
+
+          <label >Password</label>
+          <input type="password" value={password} 
+          onChange={(e) => setPassword(e.target.value)} />
+
+          <button>Create User</button>
+      </form>
+    </DefaultLayout>
+  )
 }
